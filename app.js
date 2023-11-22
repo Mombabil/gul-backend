88deee6d4fd665cdc15b58435021c565e8467834
Mombabil <mombabil.7@gmail.com>
2023-11-22 13:40:50 +0100

    test

--- app.js
+++ app.js
@@ -28,10 +28,7 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(budgetRoutes);
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://gamifyurlife.netlify.app/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
