cba8131f630f6ba3d1b7806a24b6360762f35c79
Mombabil <mombabil.7@gmail.com>
2023-11-22 14:26:12 +0100

    test

--- app.js
+++ app.js
@@ -15,13 +15,20 @@ const port = process.env.PORT || 5000;
 connectDb().catch((err) => console.log(err));
 
 // AuthorisationCORS
-app.use(
+app.use((req, res, next) => {
   cors({
-    origin: "https://gamifyurlife.netlify.app",
+    origin: "https://gamifyurlife.netlify.app/",
     credentials: true,
     optionsSuccessStatus: 200,
-  })
-);
+  });
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gamifyurlife.netlify.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // gestion des routes
 app.use(express.json());
