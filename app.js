19c424abe94934faeb8a6f0d4794b2f8a68db692
Mombabil <mombabil.7@gmail.com>
2023-11-30 09:41:26 +0100

    test

--- app.js
+++ app.js
@@ -15,20 +15,7 @@ const port = process.env.PORT || 5000;
 connectDb().catch((err) => console.log(err));
 
 // AuthorisationCORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+app.use(cors());
 
 // gestion des routes
 app.use(express.json());
